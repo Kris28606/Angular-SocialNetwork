2857ecfd194bdf97775c62af54905f97c4e195d9
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-26 21:26:04 +0200

    Kreiran model User

---
+++ src/app/model/user/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    id: number=0;
+    Username: string="";
+    Password: string="";
+    FirstName: string="";
+    LastName: string="";
+    Email: string="";
+    Token: string="";
+}
