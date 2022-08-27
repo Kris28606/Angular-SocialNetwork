8654f550dfd37c039c85ae2dc09cb61fef8ff06a
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-28 01:17:23 +0200

    Izmena

--- src/app/model/user/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    Id: number=0;
-    Username: string="";
-    Password: string="";
-    FirstName: string="";
-    LastName: string="";
-    Email: string="";
-    Token: string="";
-}
---
+++ src/app/model/user/userDto.ts
@@ -0,0 +1,9 @@
+export class UserDto {
+    id: number=0;
+    username: string="";
+    password: string="";
+    firstName: string="";
+    lastName: string="";
+    email: string="";
+    token: string="";
+}
