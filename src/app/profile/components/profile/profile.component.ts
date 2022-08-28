527bf23fc550fe319cd334249ed99cf20962f290
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-28 12:26:19 +0200

    Dodato izlistavanje korisnikovih postova

--- src/app/profile/components/profile/profile.component.ts
+++ src/app/profile/components/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Post } from 'src/app/home/components/post/model/post';
 import { UserDto } from 'src/app/model/user/userDto';
-import { TokenService } from 'src/app/token/token.service';
+import { UserService } from 'src/app/userService/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,14 +12,26 @@ import { TokenService } from 'src/app/token/token.service';
 export class ProfileComponent implements OnInit {
 
   user: UserDto=new UserDto();
+  id: number=0;
+  posts: Post[]=[];
 
-  constructor(private tokenService: TokenService) { 
+  constructor(private userService: UserService, private route: ActivatedRoute) { 
     
     console.log()
   }
-  
-
   ngOnInit(): void {
+    this.id=this.route.snapshot.params['id'];
+    this.userService.ucitajUsera(this.id).subscribe(data=> {
+      this.user=data;
+      console.log(this.user);
+    });
+
+    this.userService.GetMyPosts(this.id).subscribe(data=> {
+      this.posts=data;
+      console.log(this.posts);
+    }, error => {
+      console.log(error.message);
+    })
   }
 
 }
