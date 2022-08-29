4d3c8473db0be91e2e03a8b1c182949840798fa3
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-29 15:48:17 +0200

    Dodat atribut picture za Post

--- src/app/home/components/post/model/post.ts
+++ src/app/home/components/post/model/post.ts
@@ -1,8 +1,10 @@
 export class Post {
+    userId: number=0;
     postId: number=0;
     description: string="";
     datum!: Date;
     username: String="";
     name: String=""; 
     ago: String="";
+    picture: String="";
 }
