62fbcd8e05986e2fcb6ae54cbea7c63e3c40d73d
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-29 15:48:45 +0200

    Implementirano cuvanje Post-a

--- src/app/home/components/newPost/new-post/new-post.component.ts
+++ src/app/home/components/newPost/new-post/new-post.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UserDto } from 'src/app/model/user/userDto';
+import { PostService } from 'src/app/postService/post.service';
+import Swal from 'sweetalert2';
+import { Post } from '../../post/model/post';
 
 @Component({
   selector: 'app-new-post',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewPostComponent implements OnInit {
 
-  constructor() { }
+  @Input() user: UserDto=new UserDto();
+  post: Post=new Post();
+  file: any;
+  data=new FormData();
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
   }
 
+  savePost() {
+    this.post.userId=this.user.id;
+    console.log(this.post);
+    this.data.append('file',this.file);
+    console.log(this.data);
+    this.postService.uploadPicture(this.data).subscribe(data=> {
+      this.post.picture="https://localhost:7042/"+data.toString();
+      console.log("Slika:  "+this.post.picture);
+      this.postService.createPost(this.post).subscribe(data=> {
+      Swal.fire("Successful created post!");
+      },error=> {
+        console.log(error.message);
+      });
+    }, error => {
+      console.log(error.message);
+      return;
+    });
+  }
+
+  select(event: any) {
+    this.file=event.target.files[0];
+    console.log(this.file);
+  }
 }
+
