e6c0f17948516915fccf8ffcd9543a1a8b7cd4e3
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-29 15:46:48 +0200

    Dodate rute za kreiranje posta i upload-ovanje slike

--- src/app/postService/post.service.ts
+++ src/app/postService/post.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpContext, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Post } from '../home/components/post/model/post';
@@ -11,11 +11,19 @@ export class PostService {
 
   private baseUrl;
   constructor(private httpClient: HttpClient, private tokenService: TokenService) { 
-    this.baseUrl="https://localhost:7042";
+    this.baseUrl="https://localhost:7042/post";
   }
 
   getPostsForUser(id: number):Observable<Post[]> {
-    return this.httpClient.get<Post[]>(`${this.baseUrl}/post/all/${id}`,
+    return this.httpClient.get<Post[]>(`${this.baseUrl}/all/${id}`,
     {headers: new HttpHeaders().set('Authorization', this.tokenService.vratiToken())});
+  }
+  createPost(post: Post):Observable<Object> {
+    return this.httpClient.post(`${this.baseUrl}/new`, post,
+    {headers: new HttpHeaders().set('Authorization', this.tokenService.vratiToken()), responseType: 'arraybuffer'});
+  }
+  uploadPicture(file: FormData):Observable<Object> {
+    return this.httpClient.post(`${this.baseUrl}/upload`, file,
+    {headers: new HttpHeaders().set('Authorization', this.tokenService.vratiToken()), responseType: 'text'});
   }
 }
