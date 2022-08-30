ce20cbfbfcd05334273e7bcfbdb3363824b357a3
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-30 13:05:46 +0200

    Napravljena komponenta za pretragu

---
+++ src/app/search/search/search.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { UserDto } from 'src/app/model/user/userDto';
+import { TokenService } from 'src/app/token/token.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  @Input() user: UserDto=new UserDto();
+  ulogovani: number=0;
+  constructor(private tokenService: TokenService) { }
+
+  ngOnInit(): void {
+    this.ulogovani=this.tokenService.vratiUsera();
+  }
+
+  posaljiZahtev() {
+    console.log(this.ulogovani);
+  }
+}
