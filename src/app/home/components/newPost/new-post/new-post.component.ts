0c754c22d6c0202bd74e2fc0a34a7c6d7e83aa53
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-30 13:09:07 +0200

    Promena-pozivanje picture servisa umesto post servisa

--- src/app/home/components/newPost/new-post/new-post.component.ts
+++ src/app/home/components/newPost/new-post/new-post.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { UserDto } from 'src/app/model/user/userDto';
+import { PictureService } from 'src/app/pictureService/picture.service';
 import { PostService } from 'src/app/postService/post.service';
 import Swal from 'sweetalert2';
 import { Post } from '../../post/model/post';
@@ -15,7 +16,7 @@ export class NewPostComponent implements OnInit {
   post: Post=new Post();
   file: any;
   data=new FormData();
-  constructor(private postService: PostService) { }
+  constructor(private postService: PostService, private pictureService: PictureService) { }
 
   ngOnInit(): void {
   }
@@ -25,9 +26,11 @@ export class NewPostComponent implements OnInit {
     console.log(this.post);
     this.data.append('file',this.file);
     console.log(this.data);
-    this.postService.uploadPicture(this.data).subscribe(data=> {
+
+    this.pictureService.uploadPicture(this.data).subscribe(data=> {
       this.post.picture="https://localhost:7042/"+data.toString();
       console.log("Slika:  "+this.post.picture);
+      
       this.postService.createPost(this.post).subscribe(data=> {
       Swal.fire("Successful created post!");
       },error=> {
