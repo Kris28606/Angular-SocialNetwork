feeadcb8da72275541b4acd41e50722894addb17
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-30 13:05:09 +0200

    Izmenjena putanja

--- src/app/userService/user.service.ts
+++ src/app/userService/user.service.ts
@@ -12,16 +12,21 @@ export class UserService {
 
   private baseUrl;
   constructor(private httpClient: HttpClient, private tokenService: TokenService) { 
-    this.baseUrl="https://localhost:7042";
+    this.baseUrl="https://localhost:7042/user";
   }
 
   ucitajUsera(id: number):Observable<UserDto> {
-    return this.httpClient.get<UserDto>(`${this.baseUrl}/user/${id}`,
+    return this.httpClient.get<UserDto>(`${this.baseUrl}/${id}`,
     {headers: new HttpHeaders().set('Authorization', this.tokenService.vratiToken())});
   }
 
   GetMyPosts(id: number):Observable<Post[]> {
-    return this.httpClient.get<Post[]>(`${this.baseUrl}/user/all/${id}`,
+    return this.httpClient.get<Post[]>(`${this.baseUrl}/all/${id}`,
+    {headers: new HttpHeaders().set('Authorization', this.tokenService.vratiToken())});
+  }
+  
+  SearchUsers(kriterijum: String):Observable<UserDto[]> {
+    return this.httpClient.post<UserDto[]>(this.baseUrl+"/search", JSON.stringify(kriterijum),
     {headers: new HttpHeaders().set('Authorization', this.tokenService.vratiToken())});
   }
 }
