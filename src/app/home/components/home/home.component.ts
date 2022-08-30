461f5f0cc4433d1718def0e329b302eadc206e59
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-30 13:09:32 +0200

    Dodata pretraga user-a

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { ConstantPool } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { UserDto } from 'src/app/model/user/userDto';
 import { PostService } from 'src/app/postService/post.service';
 import { UserService } from 'src/app/userService/user.service';
@@ -16,8 +16,11 @@ export class HomeComponent implements OnInit {
   id: number=0;
   user: UserDto=new UserDto();
   posts: Post[]=[];
+  kriterijum: string="";
+  pretraga: boolean=false;
+  users: UserDto[]=[];
   constructor(private route: ActivatedRoute, private userService: UserService,
-    private postService: PostService) { }
+    private postService: PostService, private router: Router) { }
 
   ngOnInit(): void {
     this.id=this.route.snapshot.params['id'];
@@ -31,7 +34,23 @@ export class HomeComponent implements OnInit {
       console.log(this.posts);
     }, error => {
       console.log(error.message);
-    })
+    });
+  }
+
+  onChange(event : any){
+    if(this.kriterijum=="") {
+      this.pretraga=false;
+      return;
+    };
+      this.pretraga=true;
+      this.userService.SearchUsers(this.kriterijum.trim()).subscribe(data=> {
+       this.users=data;
+      console.log("Rezultat: "+data);
+      });
+  }
+
+  goToHomePage() {
+    this.router.navigate(['home', this.user.id]);
   }
 
 }
