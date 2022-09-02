bcb9ad4d7c32c045bdba9a788b4c9649c3f61297
Kris28606 <stanisavljevickristina28@gmail.com>
2022-09-02 03:39:50 +0200

    Kreirana komponenta za inbox

---
+++ src/app/chat/components/chat/chat.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserDto } from 'src/app/model/user/userDto';
+import { TokenService } from 'src/app/token/token.service';
+import { UserService } from 'src/app/userService/user.service';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit {
+
+  kriterijum: string="";
+  constructor(private router: Router,private userService: UserService, private tokenService: TokenService) { }
+
+  inboxUsers: UserDto[]=[];
+  trenutniUser: UserDto=new UserDto();
+  prikazi: boolean=false;
+  selektovaniUser: UserDto=new UserDto();
+
+
+  ngOnInit(): void {
+    this.userService.ucitajUsera(this.tokenService.vratiUsera()).subscribe(data=> {
+      this.trenutniUser=data;
+
+      this.userService.getInboxUsers(this.trenutniUser.id).subscribe(data=> {
+        this.inboxUsers=data;
+      }, error => {
+        console.log(error.message);
+      })
+
+    }, error=> {
+      console.log(error.message);
+    })
+  }
+
+  goToHomePage() {
+    this.router.navigate(['home', this.trenutniUser.id]);
+  }
+
+  onChange() {
+    
+  }
+
+  prikaziPoruke(usr: UserDto, event: any) {
+    this.selektovaniUser=usr;
+    this.prikazi=true;
+    console.log("Desilo se "+this.selektovaniUser.username);
+  }
+}
