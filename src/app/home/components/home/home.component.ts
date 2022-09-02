3d5ce49469757cc55c31dfcf60d80be7119da8a7
Kris28606 <stanisavljevickristina28@gmail.com>
2022-09-02 03:38:43 +0200

    Dodato ucitavanje user-a

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserDto } from 'src/app/model/user/userDto';
 import { PostService } from 'src/app/postService/post.service';
+import { TokenService } from 'src/app/token/token.service';
 import { UserService } from 'src/app/userService/user.service';
 import { Post } from '../post/model/post';
 
@@ -20,21 +21,24 @@ export class HomeComponent implements OnInit {
   pretraga: boolean=false;
   users: UserDto[]=[];
   constructor(private route: ActivatedRoute, private userService: UserService,
-    private postService: PostService, private router: Router) { }
+    private postService: PostService, private router: Router, private tokenService: TokenService) { }
 
   ngOnInit(): void {
-    this.id=this.route.snapshot.params['id'];
-    this.userService.ucitajUsera(this.id).subscribe(data=> {
+    this.user.username=this.tokenService.vratiUsera();
+    console.log(this.user.username);
+    this.userService.ucitajUsera(this.user.username).subscribe(data=> {
       this.user=data;
       console.log(this.user);
+        this.postService.getPostsForUser(this.user.id).subscribe(data=> {
+        this.posts=data;
+        console.log(this.posts);
+      }, error => {
+        console.log(error.message);
+      });
+
     });
 
-    this.postService.getPostsForUser(this.id).subscribe(data=> {
-      this.posts=data;
-      console.log(this.posts);
-    }, error => {
-      console.log(error.message);
-    });
+    
   }
 
   onChange(event : any){
