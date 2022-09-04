73868f49edda477bc5ff0df44d67c65bf5afd4d3
Kris28606 <stanisavljevickristina28@gmail.com>
2022-09-04 02:40:57 +0200

    Implementirane metode follow i unfollow

--- src/app/search/search/search.component.ts
+++ src/app/search/search/search.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserDto } from 'src/app/model/user/userDto';
 import { TokenService } from 'src/app/token/token.service';
+import { UserService } from 'src/app/userService/user.service';
 
 @Component({
   selector: 'app-search',
@@ -11,12 +13,24 @@ export class SearchComponent implements OnInit {
 
   @Input() user: UserDto=new UserDto();
   ulogovani: number=0;
-  constructor(private tokenService: TokenService) { }
+  constructor(private tokenService: TokenService, private router: Router, private userService: UserService) { }
 
   ngOnInit(): void {
   }
 
-  posaljiZahtev() {
-    console.log(this.ulogovani);
+  zaprati() {
+    this.userService.Follow(this.tokenService.vratiUsera(), this.user.id);
+  }
+
+  idiNaProfil() {
+    this.router.navigate(['profile', this.user.id]);
+  }
+
+  otprati() {
+    this.userService.Unfollow(this.tokenService.vratiUsera(),this.user.id).subscribe(data=> {
+      this.user.iFollow=false;
+    }, error=> {
+      console.log(error.message);
+    });
   }
 }
