fa20410e8fcf1b316ef1757c832804969fb2620d
Kris28606 <stanisavljevickristina28@gmail.com>
2022-09-07 11:20:58 +0200

    Napravljena komponenta za prikaz svih obavestenja

--- src/app/notifications/components/notification/notification.component.ts
+++ src/app/notifications/components/notification/notification.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserDto } from 'src/app/model/user/userDto';
+import { TokenService } from 'src/app/token/token.service';
+import { UserService } from 'src/app/userService/user.service';
 
 @Component({
   selector: 'app-notification',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationComponent implements OnInit {
 
-  constructor() { }
+  kriterijum: string="";
+  user: UserDto=new UserDto();
+  pretraga: boolean=false;
+  users: UserDto[]=[];
+  constructor(private router: Router, private tokenService: TokenService, private userService: UserService) { }
 
   ngOnInit(): void {
+    this.user.username=this.tokenService.vratiUsera();
+    console.log(this.user.username);
+    this.userService.ucitajUsera(this.user.username).subscribe(data=> {
+      this.user=data;
+      console.log(this.user);
+    });
+  }
+
+
+  onChange(event : any){
+    if(this.kriterijum=="") {
+      this.pretraga=false;
+      return;
+    };
+      this.pretraga=true;
+      this.userService.SearchUsers(this.kriterijum.trim(), this.user.id).subscribe(data=> {
+       this.users=data;
+      console.log("Rezultat: "+data);
+      });
+  }
+
+  goToHomePage() {
+    this.router.navigate(['home', this.user.id]);
+  }
+
+  logOut() {
+    this.tokenService.logout();
   }
 
 }
