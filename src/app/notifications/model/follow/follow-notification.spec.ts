025190b297b0486eb3ecdafecfc926ef288607ab
Kris28606 <stanisavljevickristina28@gmail.com>
2022-09-10 11:30:48 +0200

    Kreiran FollowNotification

---
+++ src/app/notifications/model/follow/follow-notification.spec.ts
@@ -0,0 +1,7 @@
+import { FollowNotification } from './follow-notification';
+
+describe('FollowNotification', () => {
+  it('should create an instance', () => {
+    expect(new FollowNotification()).toBeTruthy();
+  });
+});
---
+++ src/app/notifications/model/follow/follow-notification.ts
@@ -0,0 +1,6 @@
+export class FollowNotification {
+    fromWhoPicture: String="";
+    fromWhoUsername: String="";
+    fromWhoId: number=0;
+    date!: Date;
+}
